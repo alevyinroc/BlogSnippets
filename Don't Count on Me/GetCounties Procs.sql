deeb4efed7772bd5a6b3f88ec74b4a651f6a727c
Andy Levy <andy@levyclan.us>
2016-11-25 18:39:42 +0000

    new stored procs for post

---
+++ Don't Count on Me/GetCounties Procs.sql
@@ -0,0 +1,37 @@
+use cachedb;
+select * from states;
+select * from Countries;
+select * from counties;
+
+select s.name,c.countyname from states s join counties c on s.StateId = c.StateId order by s.name,c.CountyName;
+go
+CREATE OR ALTER PROCEDURE dbo.GetCounties
+AS
+	print 'GetCounties';
+    select s.name,c.countyname
+	from states s join counties c on s.StateId = c.StateId;
+go
+CREATE OR ALTER PROCEDURE dbo.GetCounties2
+AS
+	create table #StatesCounties (
+		StateName nvarchar(100)
+		,CountyName nvarchar(100)
+	);
+	print 'GetCounties2';
+    insert into #StatesCounties
+		select s.name as StateName,c.countyname
+		from states s join counties c on s.StateId = c.StateId;
+	select StateName,CountyName from #StatesCounties;
+go
+CREATE OR ALTER PROCEDURE dbo.GetCounties3
+AS
+	SET NOCOUNT ON
+	create table #StatesCounties (
+		StateName nvarchar(100)
+		,CountyName nvarchar(100)
+	);
+	print 'GetCounties3';
+    insert into #StatesCounties
+		select s.name as StateName,c.countyname
+		from states s join counties c on s.StateId = c.StateId;
+	select StateName,CountyName from #StatesCounties;
