52ba45491c748e7f46cefe6b154636d4217927fc
Andy Levy <andy@levyclan.us>
2016-12-22 12:41:49 -0500

    Further revisions to demo scripts

--- App Name in ConnectionString/Compare-ISC2.ps1
+++ App Name in ConnectionString/Compare-ISC2.ps1
@@ -1,6 +1,8 @@
 ﻿start-job -scriptblock {import-module C:\Users\andrewle\Documents\_GitHub\Invoke-SqlCmd2\invoke-sqlcmd2;
-Invoke-Sqlcmd2 -ServerInstance .\sql2016 -Database worldwideimporters -Query "select c.CityName from application.cities c join application.stateprovinces s on c.StateProvinceID = s.stateprovinceid join application.countries ct on ct.CountryID = s.CountryID where ct.CountryName = 'United States'  and s.StateProvinceName = 'New York'; waitfor delay '00:00:15';" -Debug -appname "Testing invoke-sqlcmd2";
+Invoke-Sqlcmd2 -ServerInstance .\sql2016 -Database worldwideimporters -Query "select c.CityName from application.cities c join application.stateprovinces s on c.StateProvinceID = s.stateprovinceid join application.countries ct on ct.CountryID = s.CountryID where ct.CountryName = 'United States'  and s.StateProvinceName = 'New York'; waitfor delay '00:00:15';" -appname "Testing invoke-sqlcmd2";
 remove-module invoke-sqlcmd2;}
+
+start-job -FilePath 'C:\Users\andrewle\documents\_github\blogsnippets\App Name in ConnectionString\test-newisc2.ps1';
 
 start-job -scriptblock {import-module C:\Users\andrewle\Documents\_GitHub\Invoke-SqlCmd2\invoke-sqlcmd2;
 Invoke-Sqlcmd2 -ServerInstance .\sql2016 -Database worldwideimporters -Query "select c.CityName from application.cities c join application.stateprovinces s on c.StateProvinceID = s.stateprovinceid join application.countries ct on ct.CountryID = s.CountryID where ct.CountryName = 'United States'  and s.StateProvinceName = 'New York'; waitfor delay '00:00:15';";
@@ -9,3 +11,5 @@ remove-module invoke-sqlcmd2;}
 start-job -scriptblock {import-module invoke-sqlcmd2;
 Invoke-Sqlcmd2 -ServerInstance .\sql2016 -Database worldwideimporters -Query "select c.CityName from application.cities c join application.stateprovinces s on c.StateProvinceID = s.stateprovinceid join application.countries ct on ct.CountryID = s.CountryID where ct.CountryName = 'United States'  and s.StateProvinceName = 'New York'; waitfor delay '00:00:15';"
 remove-module invoke-sqlcmd2;}
+
+Invoke-Sqlcmd -ServerInstance .\sql2016 -query "exec sp_whoisactive" | Select-Object -Property program_name,start_time,session_id,status|Format-Table;
---
+++ App Name in ConnectionString/Test-NewISC2a.ps1
@@ -0,0 +1,3 @@
+﻿import-module C:\Users\andrewle\Documents\_GitHub\Invoke-SqlCmd2\invoke-sqlcmd2;
+Invoke-Sqlcmd2 -ServerInstance .\sql2016 -Database worldwideimporters -Query "select c.CityName from application.cities c join application.stateprovinces s on c.StateProvinceID = s.stateprovinceid join application.countries ct on ct.CountryID = s.CountryID where ct.CountryName = 'United States'  and s.StateProvinceName = 'New York'; waitfor delay '00:00:15';";
+remove-module invoke-sqlcmd2;
