8b237adef5e2bddb2fc7982c17304c8e89bd6cb7
Andy Levy <andy@levyclan.us>
2016-12-23 12:35:37 +0000

    Missed a change

--- LeadingZerosForExcel/Import-ZIPCodes.ps1
+++ LeadingZerosForExcel/Import-ZIPCodes.ps1
@@ -4,4 +4,5 @@
 Expand-Archive -Path .\us_postal_codes.zip -DestinationPath .
 $ZipCodeData = Import-Csv -Path .\us_postal_codes.csv;
 $ZipCodeData |select -first 10 |Export-Excel -path "\\VBOXSVR\Share_to_VMWare\leadingzeroes.xlsx";
-$ZipCodeData |Select-Object -first 10 -Property @{name="Postal Code";Expression={"'$_.'Postal Code'"}},"Place Name","State","State Abbreviation","County",Latitude,Longitude |Export-Excel -Path "\\VBOXSVR\Share_to_VMWare\leadingzeroesfixed.xlsx";
+$ZipCodeData |Select-Object -first 10 -Property @{name="Postal Code";Expression={"'$($_.'Postal Code')"}},"Place Name",State,"State Abbreviation",County,Latitude,Longitude |Export-Excel -Path "\\VBOXSVR\Share_to_VMWare\leadingzeroesfixed.xlsx";
+Remove-Variable ZipcodeData
