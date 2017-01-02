bcbf40446249099ec898e4397b2acab4e6232be6
Andy Levy <andy@levyclan.us>
2017-01-01 22:10:55 -0500

    image, alternate connectionstring

--- App Name in ConnectionString/Test-ADOMethod.ps1
+++ App Name in ConnectionString/Test-ADOMethod.ps1
@@ -5,6 +5,10 @@ $DBCSBuilder['Initial Catalog'] = "WideWorldImporters";
 $DBCSBuilder['Application Name'] = "Andy's Awesome Application";
 $DBCSBuilder['Integrated Security'] = "true";
 $DBConnection.ConnectionString = $DBCSBuilder.ToString();
+
+#alternative method:
+#$DBConnection.ConnectionString = "Data Source=.\sql2016;Initial Catalog=WideWorldImporters;Integrated Security=true;Application name=Andy's Awesome Application;"
+
 $DBConnection.Open();
 $QueryCmd = $DBConnection.CreateCommand();
 $QueryCmd.CommandText = "select c.CityName from application.cities c join application.stateprovinces s on c.StateProvinceID = s.stateprovinceid join application.countries ct on ct.CountryID = s.CountryID where ct.CountryName = 'United States'  and s.StateProvinceName = 'New York'; waitfor delay '00:00:15';";
